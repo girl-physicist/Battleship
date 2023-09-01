cb28c6392da24c97fbac3543487b6af42320d076
Anastasiya <girl-physicist@mail.ru>
2023-09-01 20:08:38 +0300

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Input a");
+        int c = 0;
+        int a = Convert.ToInt16 (Console.ReadLine());
+        Console.WriteLine("Input b");
+        int b = Convert.ToInt16(Console.ReadLine());
+        c= a+b; 
+        Console.WriteLine("a+b={0}", c);
+    }
+}
